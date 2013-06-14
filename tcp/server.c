4162c0608bea171eee74f26dffc8a54f772753a6
Yuri Campos <yuricampos@MacBook-Pro-de-Yuri.local>
2013-06-14 13:41:09 -0300

    criacao tcp

---
+++ tcp/server.c
@@ -0,0 +1,125 @@
+/********************************************************copyrite.xic***/
+/*                                                                     */
+/*   Licensed Materials - Property of IBM                              */
+/*                                                                     */
+/*   This product contains "Restricted Materials of IBM":              */
+/*      Program Number:   5798RXW                                      */
+/*      Program Name:     IBM TCP/IP Version 1.2 for OS/2              */
+/*   (C) Copyright IBM Corporation. 1990, 1991.                        */
+/*                                                                     */
+/*   All rights reserved.                                              */
+/*                                                                     */
+/*   US Government Users Restricted Rights -                           */
+/*   Use, duplication or disclosure restricted by GSA ADP Schedule     */
+/*   Contract with IBM Corp.                                           */
+/*                                                                     */
+/*   See IBM Copyright Instructions.                                   */
+/*                                                                     */
+/********************************************************copyrite.xic***/
+/*
+ * Include Files.
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+/*
+ * Server Main.
+ */
+main(argc, argv)
+int argc;
+char **argv;
+{
+    unsigned short port;       /* port server binds to                  */
+    char buf[12];              /* buffer for sending and receiving data */
+    struct sockaddr_in client; /* client address information            */
+    struct sockaddr_in server; /* server address information            */
+    int s;                     /* socket for accepting connections      */
+    int ns;                    /* socket connected to client            */
+    int namelen;               /* length of client name                 */
+    
+    /*
+     * Check arguments. Should be only one: the port number to bind to.
+     */
+    
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s port\n", argv[0]);
+        exit(1);
+    }
+    
+    /*
+     * First argument should be the port.
+     */
+    port = (unsigned short) atoi(argv[1]);
+    
+    /*
+     * Get a socket for accepting connections.
+     */
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket()");
+        exit(2);
+    }
+    
+    /*
+     * Bind the socket to the server address.
+     */
+    server.sin_family = AF_INET;
+    server.sin_port   = htons(port);
+    server.sin_addr.s_addr = INADDR_ANY;
+    
+    if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("Bind()");
+        exit(3);
+    }
+    
+    /*
+     * Listen for connections. Specify the backlog as 1.
+     */
+    if (listen(s, 1) != 0)
+    {
+        perror("Listen()");
+        exit(4);
+    }
+    
+    /*
+     * Accept a connection.
+     */
+    namelen = sizeof(client);
+    if ((ns = accept(s, (struct sockaddr *)&client, &namelen)) == -1)
+    {
+        perror("Accept()");
+        exit(5);
+    }
+    
+    /*
+     * Receive the message on the newly connected socket.
+     */
+    if (recv(ns, buf, sizeof(buf), 0) == -1)
+    {
+        perror("Recv()");
+        exit(6);
+    }
+        
+        printf("Mensagem: %s \n",buf);
+    strcpy(buf, "from server");
+    
+    /*
+     * Send the message back to the client.
+     */
+    if (send(ns, buf, sizeof(buf), 0) < 0)
+    {
+        perror("Send()");
+        exit(7);
+    }
+    
+    close(ns);
+    close(s);
+    printf("Server ended successfully\n");
+    exit(0);
+}
+
